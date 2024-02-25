c5fb908245ab0742be4520ff7d019a2478a39214
thirukumaran05 <learningthiru@gmail.com>
2024-02-25 19:15:53 +0530

    Added Rules

---
+++ public/main.js
@@ -0,0 +1,17 @@
+const rock  = document.querySelector('.choice__rock');
+const paper  = document.querySelector('.choice__paper');
+const scissor  = document.querySelector('.choice__scissor');
+
+const rulesBtn = document.querySelector('.rules__button');
+const rulesBoard = document.querySelector('.rules');
+const showRulesBoard = document.querySelector('.show__result_board')
+const closeRules = document.querySelector('.close-btn');
+
+rulesBtn.addEventListener('click', () => {
+    rulesBoard.classList.add('show__result_board');
+    closeRules.style.cursor = 'pointer';
+});
+
+closeRules.addEventListener('click', () => {
+    rulesBoard.classList.toggle('show__result_board');
+});
